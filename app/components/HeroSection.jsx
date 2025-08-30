f8df80f495de828aee109f628a0a08f7b4385db3
BarbaraMonH <barbara.mondragon.herrada@gmail.com>
2025-08-29 21:31:25 -0600

    New projects

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -5,10 +5,10 @@ import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
   return (
-    <section>
+    <section className=''>
         <div className="grid grid-cols-1 sm:grid-cols-12">
-            <div className="col-span-7 place-self-center text-center sm:text-left">
-                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold"> 
+            <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold"> 
                     <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-700 via-white to-orange-700">
                         Hello, I'm{" "} 
                     </span>
@@ -36,7 +36,7 @@ const HeroSection = () => {
                     <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>CV</button>
                 </div>
             </div>
-            <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+            <div className='col-span-4 place-self-center mt-4 lg:mt-0'>
                 <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                     <Image
                         src="/images/Avatar.png"
--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -6,7 +6,7 @@ import ProjectTag from "./ProjectTag";
 const ProjectData = [
     {
         id: 1,
-        title: "Breast abnormality classifier based on mammograms",
+        title: "Fusibles",
         description: "Project 1 description",
         image: "/images/Projects/1.png",
         tag: ["All", "Machine Learning"],
@@ -14,20 +14,44 @@ const ProjectData = [
     },
     {
         id: 2,
-        title: "Breast abnormality classifier based on mammograms",
-        description: "Project 1 description",
-        image: "/images/Projects/1.png",
+        title: "Development and Evaluation of an Integrated Tangible, Augmented, and Virtual Reality System for Professional Medical Training",
+        description: "Project 2 description",
+        image: "/images/Projects/2.png",
         tag: ["All", "Mixed and Virtual Reality"],
         previewUrl: "/"
     },
     {
         id: 3,
-        title: "Breast abnormality classifier based on mammograms",
-        description: "Project 1 description",
+        title: "Virtual reality-based system for training in the use of transhumeral prostheses",
+        description: "Project 3 description",
+        image: "/images/Projects/3.png",
+        tag: ["All", "App Development"],
+        previewUrl: "/"
+    },
+    {
+        id: 4,
+        title: "Development of soft robots for embodied education of proprioception in early childhood",
+        description: "Project 3 description",
         image: "/images/Projects/1.png",
         tag: ["All", "App Development"],
         previewUrl: "/"
-    }
+    },
+    {
+        id: 5,
+        title: "Breast abnormality classifier based on mammograms",
+        description: "Project 4 description",
+        image: "/images/Projects/1.png",
+        tag: ["All", "Machine Learning"],
+        previewUrl: "/"
+    },
+    {
+        id: 6,
+        title: "App SoyBienestar",
+        description: "Project 6 description",
+        image: "/images/Projects/6.png",
+        tag: ["All", "Machine Learning"],
+        previewUrl: "/"
+    },
 ]
 
 const ProjectsSection = () => {
@@ -68,17 +92,19 @@ const ProjectsSection = () => {
                 isSelected={tag === "App Development"} 
             />
         </div>
+        
         <div className="grid md:grid-cols-3 gap-8 md:gap-12">
             {filteredProjects.map((project) => (
-            <ProjectCard 
-                key={project.id} 
-                title={project.title} 
-                description={project.description} 
+            <ProjectCard
+                key={project.id}
+                title={project.title}
+                description={project.description}
                 imgUrl={project.image}
                 tags = {project}
                 previewUrl={project.previewUrl}
             />
-        ))}</div></>
+        ))}</div>
+        </>
     )
 }
 
